60bf0e5858dfb4286cca1ec6150d48da14caae1b
Greg Poirier <grep@binary-snobbery.com>
2015-08-06 14:52:32 -0700

    Delete crufty scripts

--- scripts/docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t gozer-build -f Dockerfile.build .
-docker run --rm --volume `pwd`/export:/export gozer-build
-docker build -t quay.io/opsee/vinz -f Dockerfile.vinz .
-
